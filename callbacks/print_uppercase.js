de02b2cb0a47173554fc733019dee6e511a5d872
zuzanatoldyova <zuzana.toldyova@gmail.com>
2017-02-13 20:26:48 +0000

    print uppercase function working

---
+++ callbacks/print_uppercase.js
@@ -0,0 +1,13 @@
+var getHTML = require('../http-functions');
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step6/uppercase.html'
+};
+
+function printUpperCase (html) {
+  console.log(html.toUpperCase());
+
+}
+
+getHTML(requestOptions, printUpperCase);
