aa741a474aa2b10caba02f31ec33670fb351f018
zuzanatoldyova <zuzana.toldyova@gmail.com>
2017-02-13 21:38:54 +0000

    lowercase characters working

---
+++ callbacks/print_lowercase.js
@@ -0,0 +1,13 @@
+var getHTML = require('../http-functions');
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step6/lowercase.html'
+};
+
+function printLowerCase (html) {
+  console.log(html.toLowerCase());
+
+}
+
+getHTML(requestOptions, printLowerCase);
